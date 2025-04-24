e335f8b00f755347a0685c87255f4b708d06c352
Duc32023 <86730180+Duc32023@users.noreply.github.com>
2025-04-24 13:26:17 +0700

    sss

--- App.js
+++ App.js
@@ -4,12 +4,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import RouteStack from './exercise/RouteStack';
 import Store from './exercise/Store';
+import Login from './exercise/Login';
+import ForgetPassword from './exercise/ForgetPassword';
+import Register from './exercise/Register';
+im
 export default function App() {
   return (
     <Store>
     <PaperProvider>
     <NavigationContainer>
-      <RouteStack />
+     <Login/>
+      {/* <ForgetPassword/> */}
+      {/* <Register/> */}
+      {/* <RouteStack/> */}
     </NavigationContainer>
   </PaperProvider>
   </Store>
