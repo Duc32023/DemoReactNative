a1b29f6948dc784a5a8373bf010c580e41d7e0cb
Duc32023 <86730180+Duc32023@users.noreply.github.com>
2025-04-25 08:13:03 +0700

    fix r ne

--- App.js
+++ App.js
@@ -7,16 +7,14 @@ import Store from './exercise/Store';
 import Login from './exercise/Login';
 import ForgetPassword from './exercise/ForgetPassword';
 import Register from './exercise/Register';
-im
+
 export default function App() {
   return (
     <Store>
     <PaperProvider>
     <NavigationContainer>
-     <Login/>
-      {/* <ForgetPassword/> */}
-      {/* <Register/> */}
-      {/* <RouteStack/> */}
+
+      <RouteStack/>
     </NavigationContainer>
   </PaperProvider>
   </Store>
