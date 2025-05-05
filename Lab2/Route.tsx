40f57ff1f9b36617a70c82fd966d9af858ee90cf
Duc32023 <86730180+Duc32023@users.noreply.github.com>
2025-05-05 23:15:46 +0700

    Lab2

--- Lab2/Route.tsx
+++ Lab2/Route.tsx
@@ -1,50 +1,121 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { fetchRandomContact } from '../utility/api';
-import ContactThumbnail from './ContactThumbnail'
-import DetailListItem from './DetailListItem';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Contacts from './Contacts'
+import Profile from './Profile';
 import colors from '../utility/colors';
-import { NativeStackScreenProps } from '@react-navigation/native-stack/lib/typescript/src';
 import { ContactParamList } from '../type/type';
-interface Contact{
-    avatar: string;
-    name: string;
-    email: string;
-    phone: string;
-    cell: string;
-}
-type ProfileProps = NativeStackScreenProps<ContactParamList, 'Profile'>;
-const Profile:React.FC<ProfileProps> = ({route}) => {
-  const { contact } = route.params;
-  const { avatar, name, email, phone, cell } = contact as Contact;
+import Favorites from './Favorite';
+import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import User from './User';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Options from './Options';
+type UserScreenProps = StackScreenProps<ContactParamList, 'User'>;
+const Stack = createNativeStackNavigator<ContactParamList>();
+const getTabBarIcon = (icon: any) => ({ color }: { color: string }) => (
+  <Icon name={icon} size={26} color={color} />
+);
+
+const ContactsScreens  = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.avatarSection}>
-        <ContactThumbnail avatar={avatar} name={name} phone={phone} />
-      </View>
-      <View style={styles.detailsSection}>
-        <DetailListItem icon="mail" title="Email" subtitle={email} />
-        <DetailListItem icon="phone" title="Work" subtitle={phone} />
-        <DetailListItem icon="smartphone" title="Personal" subtitle={cell} />
-      </View>
-    </View>
+    
+      <Stack.Navigator
+        initialRouteName="Contacts"
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'tomato' },
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen
+        name="Contacts"
+        component={Contacts}
+        options={{ title: 'Contacts' }}
+      />
+       
+        <Stack.Screen
+        name="Profile"
+        component={Profile}
+//route trong options={({ route }) => {...}} là tham số được React Navigation cung cấp truy cập thông tin của màn hình.
+// route trong const { contact } = route.params ở profile sử dụng dữ liệu từ route để trích xuất thông tin cụ thể.
+// Hai lần xuất hiện của route là ở hai phạm vi khác nhau: một là tham số hàm, một là truy cập thuộc tính.
+        options={({ route: { params: { contact: { name } } } }) => ({
+          title: name.split(' ')[0],
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: colors.blue },
+        })}
+      />
+      </Stack.Navigator>
+   
+  );
+};
+const FavoritesScreens = () => {
+  return (
+    <Stack.Navigator initialRouteName="Favorite">
+      <Stack.Screen
+        name="Favorite"
+        component={Favorites}
+        options={{ title: 'Favorites' }}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{ title: 'Profile' }}
+      />
+     
+    </Stack.Navigator>
   );
 };
 
-export default Profile;
+const UserScreens = ({ navigation }:UserScreenProps) => {
+  return (
+    <Stack.Navigator initialRouteName="User">
+      <Stack.Screen name="User" component={User} />
+      <Stack.Screen name="Options" component={Options} options={{ title: 'Options' }} />
+    </Stack.Navigator>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  avatarSection: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.blue,
-  },
-  detailsSection: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-});
+const Tab = createMaterialBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="ContactsScreens"
+        barStyle={{
+          backgroundColor: colors.blue,
+        }}
+        labeled={true}
+        activeTintColor={colors.greyLight}
+        inactiveColor={colors.greyDark}
+      >
+        <Tab.Screen
+          name="ContactsScreens"
+          component={ContactsScreens}
+          options={{
+            tabBarIcon: getTabBarIcon('list'),
+          }}
+        />
+        <Tab.Screen
+          name="FavoritesScreens"
+          component={FavoritesScreens}
+          options={{
+            tabBarIcon: getTabBarIcon('star'),
+          }}
+        />
+        <Tab.Screen
+          name="UserScreens"
+          component={UserScreens}
+          options={{
+            tabBarIcon: getTabBarIcon('person'),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default TabNavigator;
